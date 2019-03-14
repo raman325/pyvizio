692ed91ec6359ef1df463e81f81a15d11f33d18b
Raman Gupta <raman325@gmail.com>
2019-03-14 01:09:09 -0400

    Updated HA component to match HA version and to add sound bar support
    All changes were intended to be backwards compatible
    - Added new device class parameter to choose between tv and sound bar (defaults to tv)
    - Change volume scaling (my sound bar maxes out at 31 instead of 100) and supported features (couldn't find keys to control previous and next track) based on device class
    - Added play and pause support

--- custom_components/media_player/vizio.py
+++ custom_components/media_player/vizio.py
@@ -1,145 +1,232 @@
-"""Vizio SmartCast TV support"""
-import logging
+"""
+Vizio SmartCast TV + Sound Bar support.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/media_player.vizio/
+"""
 from datetime import timedelta
+import logging
 
 import voluptuous as vol
 
-import homeassistant.util as util
+from homeassistant import util
 from homeassistant.components.media_player import (
-    PLATFORM_SCHEMA,
-    SUPPORT_TURN_ON,
-    SUPPORT_TURN_OFF,
-    SUPPORT_SELECT_SOURCE,
-    SUPPORT_PREVIOUS_TRACK,
-    SUPPORT_NEXT_TRACK,
-    SUPPORT_VOLUME_MUTE,
-    SUPPORT_VOLUME_STEP,
-    MediaPlayerDevice
-)
+    MediaPlayerDevice, PLATFORM_SCHEMA)
+from homeassistant.components.media_player.const import (
+    SUPPORT_NEXT_TRACK, SUPPORT_PREVIOUS_TRACK,
+    SUPPORT_PLAY, SUPPORT_PAUSE,
+    SUPPORT_SELECT_SOURCE, SUPPORT_TURN_OFF, SUPPORT_TURN_ON,
+    SUPPORT_VOLUME_MUTE, SUPPORT_VOLUME_SET, SUPPORT_VOLUME_STEP)
 from homeassistant.const import (
-    STATE_UNKNOWN,
-    STATE_OFF,
-    STATE_ON,
-    CONF_NAME,
-    CONF_HOST,
-    CONF_ACCESS_TOKEN
-)
+    CONF_ACCESS_TOKEN, CONF_HOST, CONF_NAME, CONF_DEVICE_CLASS,
+    STATE_OFF, STATE_ON)
 from homeassistant.helpers import config_validation as cv
 
-REQUIREMENTS = ['git+https://github.com/vkorn/pyvizio.git'
-                '@master#pyvizio==0.0.1']
+REQUIREMENTS = ['pyvizio==0.0.4']
 
 _LOGGER = logging.getLogger(__name__)
 
-ICON = 'mdi:television'
+CONF_SUPPRESS_WARNING = 'suppress_warning'
+CONF_VOLUME_STEP = 'volume_step'
+
 DEFAULT_NAME = 'Vizio SmartCast'
-DEVICE_NAME = 'Python Vizio'
+DEFAULT_VOLUME_STEP = 1
+DEFAULT_DEVICE_CLASS = 'tv'
 DEVICE_ID = 'pyvizio'
-MIN_TIME_BETWEEN_SCANS = timedelta(seconds=10)
+DEVICE_NAME = 'Python Vizio'
+
+ICON = 'mdi:television'
+
 MIN_TIME_BETWEEN_FORCED_SCANS = timedelta(seconds=1)
-SUPPORTED_COMMANDS = SUPPORT_TURN_ON | SUPPORT_TURN_OFF | SUPPORT_SELECT_SOURCE \
-                     | SUPPORT_NEXT_TRACK | SUPPORT_PREVIOUS_TRACK \
-                     | SUPPORT_VOLUME_MUTE | SUPPORT_VOLUME_STEP
+MIN_TIME_BETWEEN_SCANS = timedelta(seconds=10)
+
+ALLOWED_DEVICE_CLASS = ['tv', 'soundbar']
+
+SUPPORTED_COMMANDS['tv'] = \
+                        SUPPORT_TURN_ON | SUPPORT_TURN_OFF \
+                        | SUPPORT_PLAY | SUPPORT_PAUSE
+                        | SUPPORT_SELECT_SOURCE \
+                        | SUPPORT_NEXT_TRACK | SUPPORT_PREVIOUS_TRACK \
+                        | SUPPORT_VOLUME_MUTE | SUPPORT_VOLUME_STEP \
+                        | SUPPORT_VOLUME_SET
+
+SUPPORTED_COMMANDS['soundbar'] = \
+                        SUPPORT_TURN_ON | SUPPORT_TURN_OFF \
+                        | SUPPORT_PLAY | SUPPORT_PAUSE
+                        | SUPPORT_SELECT_SOURCE \
+                        | SUPPORT_VOLUME_MUTE | SUPPORT_VOLUME_STEP \
+                        | SUPPORT_VOLUME_SET
+
+MAX_VOLUME['tv'] = 100.0
+MAX_VOLUME['soundbar'] = 31.0
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
-    vol.Required(CONF_ACCESS_TOKEN): cv.string,
+    vol.Optional(CONF_ACCESS_TOKEN): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Optional(CONF_SUPPRESS_WARNING, default=False): cv.boolean,
+    vol.Optional(CONF_DEVICE_CLASS, default=DEFAULT_DEVICE_CLASS): 
+        vol.All(cv.string, vol.In(ALLOWED_DEVICE_CLASS)),
+    vol.Optional(CONF_VOLUME_STEP, default=DEFAULT_VOLUME_STEP):
+        vol.All(vol.Coerce(int), vol.Range(min=1, max=10)),
 })
 
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the Vizio media player platform."""
     host = config.get(CONF_HOST)
-    if host is None:
-        _LOGGER.error('No host info')
-        return False
     token = config.get(CONF_ACCESS_TOKEN)
-    if token is None:
-        _LOGGER.error('No token info')
-        return False
     name = config.get(CONF_NAME)
+    volume_step = config.get(CONF_VOLUME_STEP)
+    device_type = config.get(CONF_DEVICE_CLASS)
 
-    add_devices([VizioDevice(host, token, name)], True)
+    device = VizioDevice(host, token, name, volume_step, device_type)
+    if device.validate_setup() is False:
+        _LOGGER.error("Failed to set up Vizio platform, "
+                      "please check if host and API key are correct")
+        return
+
+    if config.get(CONF_SUPPRESS_WARNING):
+        from requests.packages import urllib3
+        _LOGGER.warning("InsecureRequestWarning is disabled "
+                        "because of Vizio platform configuration")
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    add_entities([device], True)
 
 
 class VizioDevice(MediaPlayerDevice):
-    def __init__(self, host, token, name):
+    """Media Player implementation which performs REST requests to device."""
+    def __init__(self, host, token, name, device_type, volume_step):
+        """Initialize Vizio device."""
         import pyvizio
-        self._device = pyvizio.Vizio(DEVICE_ID, host, DEFAULT_NAME, token)
+        self._device = pyvizio.Vizio(DEVICE_ID, host, DEFAULT_NAME, device_type, token)
         self._name = name
-        self._state = STATE_UNKNOWN
+        self._state = None
         self._volume_level = None
+        self._volume_step = volume_step
         self._current_input = None
         self._available_inputs = None
+        self._device_type = device_type
+        self._max_volume = MAX_VOLUME[device_type]
+        self._supported_commands = SUPPORTED_COMMANDS[device_type]
 
     @util.Throttle(MIN_TIME_BETWEEN_SCANS, MIN_TIME_BETWEEN_FORCED_SCANS)
     def update(self):
+        """Retrieve latest state of the device."""
         is_on = self._device.get_power_state()
-        if is_on is None:
-            self._state = STATE_UNKNOWN
-            return
-        elif is_on is False:
-            self._state = STATE_OFF
-        else:
+
+        if is_on:
             self._state = STATE_ON
 
-        self._volume_level = self._device.get_current_volume()
-        input_ = self._device.get_current_input()
-        if input_ is not None:
-            self._current_input = input_.meta_name
-        inputs = self._device.get_inputs()
-        if inputs is not None:
-            self._available_inputs = []
-            for input_ in inputs:
-                self._available_inputs.append(input_.name)
+            volume = self._device.get_current_volume()
+            if volume is not None:
+                self._volume_level = float(volume) / self._max_volume
+
+            input_ = self._device.get_current_input()
+            if input_ is not None:
+                self._current_input = input_.meta_name
+
+            inputs = self._device.get_inputs()
+            if inputs is not None:
+                self._available_inputs = [input_.name for input_ in inputs]
+
+        else:
+            if is_on is None:
+                self._state = None
+            else:
+                self._state = STATE_OFF
+
+            self._volume_level = None
+            self._current_input = None
+            self._available_inputs = None
 
     @property
     def state(self):
+        """Return the state of the device."""
         return self._state
 
     @property
     def name(self):
+        """Return the name of the device."""
         return self._name
 
     @property
     def volume_level(self):
+        """Return the volume level of the device."""
         return self._volume_level
 
     @property
     def source(self):
+        """Return current input of the device."""
         return self._current_input
 
     @property
     def source_list(self):
+        """Return list of available inputs of the device."""
         return self._available_inputs
 
     @property
     def supported_features(self):
-        return SUPPORTED_COMMANDS
+        """Flag device features that are supported."""
+        return self._supported_commands
 
     def turn_on(self):
+        """Turn the device on."""
         self._device.pow_on()
 
     def turn_off(self):
+        """Turn the device off."""
         self._device.pow_off()
 
     def mute_volume(self, mute):
+        """Mute the volume."""
         if mute:
             self._device.mute_on()
         else:
             self._device.mute_off()
 
+    def media_play(self):
+        """Send play command."""
+        self._device.play()
+    
+    def media_pause(self):
+        """Send pause command."""
+        self._device.pause()
+
     def media_previous_track(self):
+        """Send previous channel command."""
         self._device.ch_down()
 
     def media_next_track(self):
+        """Send next channel command."""
         self._device.ch_up()
 
     def select_source(self, source):
+        """Select input source."""
         self._device.input_switch(source)
 
     def volume_up(self):
-        self._device.vol_up()
+        """Increasing volume of the device."""
+        self._volume_level += self._volume_step / self._max_volume
+        self._device.vol_up(num=self._volume_step)
 
     def volume_down(self):
-        self._device.vol_down()
+        """Decreasing volume of the device."""
+        self._volume_level -= self._volume_step / self._max_volume
+        self._device.vol_down(num=self._volume_step)
+
+    def validate_setup(self):
+        """Validate if host is available and key is correct."""
+        return self._device.get_current_volume() is not None
+
+    def set_volume_level(self, volume):
+        """Set volume level."""
+        if self._volume_level is not None:
+            if volume > self._volume_level:
+                num = int(self._max_volume*(volume - self._volume_level))
+                self._volume_level = volume
+                self._device.vol_up(num=num)
+            elif volume < self._volume_level:
+                num = int(self._max_volume*(self._volume_level - volume))
+                self._volume_level = volume
+                self._device.vol_down(num=num)
