04dc017a00f3443b728698b1e94e012bfa2f5e0d
Raman Gupta <7243222+raman325@users.noreply.github.com>
2020-01-18 14:26:08 -0500

    disable flake8 line length test

--- pyvizio/__init__.py
+++ pyvizio/__init__.py
@@ -1,1 +1,6 @@
-from .vizio import Vizio, VizioAsync, async_guess_device_type, guess_device_type
+from .vizio import (  # noqa: F401
+    Vizio,
+    VizioAsync,
+    async_guess_device_type,
+    guess_device_type,
+)
