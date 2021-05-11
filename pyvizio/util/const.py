8992abec8808b89e6aa5873d8c12c78c8a9c9f64
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-05-11 01:08:39 -0400

    Add comments so I know where to find apps files in the future

--- pyvizio/util/const.py
+++ pyvizio/util/const.py
@@ -4,7 +4,10 @@ APK_SOURCE_PATH = "src"
 RESOURCE_PATH = "resources/res/raw"
 APP_NAMES_FILE = "apps.json"
 APP_PAYLOADS_FILE = "apps_availability.json"
+# smartcast.apk-decompiled\res\values\strings.xml
+# <string name="default_appsservice_app_server">
 APP_NAMES_URL = "http://hometest.buddytv.netdna-cdn.com/appservice/vizio_apps_prod.json"
+# <string name="default_appsservice_availability_server">
 APP_PAYLOADS_URL = (
     "http://hometest.buddytv.netdna-cdn.com/appservice/app_availability_prod.json"
 )
