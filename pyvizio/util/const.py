96862be29a968f20b215f1867b83e8a24dc3833b
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-05-11 01:09:43 -0400

    comments

--- pyvizio/util/const.py
+++ pyvizio/util/const.py
@@ -4,7 +4,10 @@ APK_SOURCE_PATH = "src"
 RESOURCE_PATH = "resources/res/raw"
 APP_NAMES_FILE = "apps.json"
 APP_PAYLOADS_FILE = "apps_availability.json"
-# smartcast.apk-decompiled\res\values\strings.xml
+
+# File with app URLs: smartcast.apk-decompiled\res\values\strings.xml
+# Use the keys below to find the values
+
 # <string name="default_appsservice_app_server">
 APP_NAMES_URL = "http://hometest.buddytv.netdna-cdn.com/appservice/vizio_apps_prod.json"
 # <string name="default_appsservice_availability_server">
